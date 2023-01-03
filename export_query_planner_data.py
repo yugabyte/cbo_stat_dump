13ca9c2981e9d2a6c8f3b82ae7409822d4468028
Dmitry Sherstobitov <sdn.xaoc@gmail.com>
2023-01-03 16:38:12 +0400

    Reformat export_query_planner_data.py

--- export_query_planner_data.py
+++ export_query_planner_data.py
@@ -14,38 +14,43 @@ options:
   -o OUT_DIR, --out_dir OUT_DIR
   -q SQL_FILE, --sql_file SQL_FILE
 '''
-from _ctypes import PyObj_FromPtr
-import psycopg2
 import argparse
-import time
-import os, platform, subprocess, sys
-from pathlib import Path
-import shutil
-import re
 import json
+import os
+import re
+import shutil
+import subprocess
+import sys
+import time
+
+from _ctypes import PyObj_FromPtr
+from pathlib import Path
+
+import psycopg2
+
 
 def get_connection_dict(args):
-    envOpts = os.environ
     host = args.host
     port = args.port
     user = args.user
     password = args.password
-    db = args.database;
-    print ("Connecting to database host=%s, port=%s, user=%s, db=%s" % (host, port, user, db))
+    db = args.database
+    print(f"Connecting to database host={host}, port={port}, user={user}, db={db}")
 
-    connectionDict = {
+    return {
         'host': host,
         'port': port,
         'user': user,
         'password': password,
         'database': db
     }
-    return connectionDict
 
-def connect_database(connectionDict):
-    conn = psycopg2.connect(**connectionDict)
+
+def connect_database(connection_dict):
+    conn = psycopg2.connect(**connection_dict)
     cursor = conn.cursor()
     return conn, cursor
+
 
 QUERY_FILE_NAME = 'query.sql'
 DDL_FILE_NAME = 'ddl.sql'
@@ -56,22 +61,30 @@ DEFAULT_OUT_DIR_PREFIX = 'query_planner_data_'
 
 STATISTICS_FETCH_ROWS_SIZE = 1000
 
+
 def parse_cmd_line():
     parser = argparse.ArgumentParser(
-        prog = 'export_query_planner_data',
-        description = 'Exports statistics and other data to reproduce query plan'
+        prog='export_query_planner_data',
+        description='Exports statistics and other data to reproduce query plan'
     )
-    parser.add_argument('-H', '--host', help='Hostname or IP address, default localhost', default="localhost")
+    parser.add_argument('-H', '--host', help='Hostname or IP address, default localhost',
+                        default="localhost")
     parser.add_argument('-P', '--port', help='Port number, default 5433', default=5433)
-    parser.add_argument('-D', '--database', required=True, help='Database name, default yugabyte', default="yugabyte")
-    parser.add_argument('-u', '--user', required=True, help='YugabyteDB username, default yugabyte', default="yugabyte")
+    parser.add_argument('-D', '--database', required=True, help='Database name, default yugabyte',
+                        default="yugabyte")
+    parser.add_argument('-u', '--user', required=True, help='YugabyteDB username, default yugabyte',
+                        default="yugabyte")
     parser.add_argument('-p', '--password', help='Password, default no password')
-    parser.add_argument('-o', '--out_dir', default='/tmp/' + DEFAULT_OUT_DIR_PREFIX +time.strftime("%Y%m%d-%H%M%S"), help='Output directory')
+    parser.add_argument('-o', '--out_dir',
+                        default='/tmp/' + DEFAULT_OUT_DIR_PREFIX + time.strftime("%Y%m%d-%H%M%S"),
+                        help='Output directory')
     parser.add_argument('-q', '--sql_file')
-    parser.add_argument('--enable_optimizer_statistics', action=argparse.BooleanOptionalAction, help='Set yb_enable_optimizer_statistics=ON before running explain on query')
+    parser.add_argument('--enable_optimizer_statistics', action=argparse.BooleanOptionalAction,
+                        help='Set yb_enable_optimizer_statistics=ON before running explain on query')
 
     args = parser.parse_args()
     return args
+
 
 def get_relations_from_json_recurse(query_plan_json):
     relations = []
@@ -82,13 +95,19 @@ def get_relations_from_json_recurse(query_plan_json):
             relations = relations + (get_relations_from_json_recurse(sub_plan_json))
     return relations
 
+
 def get_relation_names_in_query(cursor, sql_file):
     with open(sql_file, 'r') as sql_f:
         sql_text = sql_f.read()
-    
-    explain_query = "EXPLAIN (FORMAT JSON) %s" % sql_text
-    cursor.execute(explain_query)
-    query_plan_json = cursor.fetchone()[0][0]
+    explain_query = f"EXPLAIN (FORMAT JSON) {sql_text}"
+
+    try:
+        cursor.execute(explain_query)
+        query_plan_json = cursor.fetchone()[0][0]
+    except Exception as e:
+        print(f"Failed to evaluate:\n{explain_query}")
+        raise e
+
     relations = []
     if 'Relation Name' in query_plan_json['Plan']:
         relations.append(query_plan_json['Plan']['Relation Name'])
@@ -96,85 +115,94 @@ def get_relation_names_in_query(cursor, sql_file):
     relations = list(dict.fromkeys(relations))
     return relations
 
+
 def get_relation_oids(cursor, relation_names):
     relation_oids = []
     for relation_name in relation_names:
-        oid_query = "SELECT oid FROM pg_class WHERE relname='%s'" % relation_name
+        oid_query = f"SELECT oid FROM pg_class WHERE relname='{relation_name}'"
         cursor.execute(oid_query)
         relation_oids.append(cursor.fetchone()[0])
     return relation_oids
 
+
 def get_process_output(cmd):
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                         env=os.environ)
     outmsg, errormsg = p.communicate()
     if p.returncode != 0:
-        sys.stderr.writelines('\nError when executing the following command.\n\n' + cmd + '\n' + errormsg.decode() + '\n\n')
+        sys.stderr.writelines(
+            '\nError when executing the following command.\n\n' + cmd + '\n' + errormsg.decode() + '\n\n')
         sys.exit(1)
     return outmsg.decode('UTF-8')
 
-def extract_ddl(cursor, connectionDict, relation_names, output_dir):
+
+def extract_ddl(connection_dict, relation_names, output_dir):
     if not shutil.which("ysql_dump"):
         sys.stderr.writelines("ysql_dump binary not found.\n\n")
         sys.exit(1)
     
-    ddl_file_name = output_dir + '/' + DDL_FILE_NAME
+    ddl_file_name = f'{output_dir}/{DDL_FILE_NAME}'
     print("Exporting DDL to %s" % ddl_file_name)
     ddl_tmp_file_name = ddl_file_name + '.tmp'
-    
-    ysql_connection_str = "-d %s -h %s -p %s -U %s" % (connectionDict["database"], connectionDict["host"], connectionDict["port"], connectionDict["user"])
-    password_prefix_str = ""
-    if connectionDict["password"] is not None:
-        password_prefix_str = "PGPASSWORD=%s " % connectionDict["password"]
+    ysql_connection_str = "-d %s -h %s -p %s -U %s" % (connection_dict["database"], connection_dict["host"], connection_dict["port"], connection_dict["user"])
 
+    password_prefix_str = ""
+    if connection_dict["password"] is not None:
+        password_prefix_str = f'PGPASSWORD={connection_dict["password"]} '
     if relation_names:
         for relation_name in relation_names:
-            ysql_dump_cmd = password_prefix_str + "ysql_dump %s -t %s -s" % (ysql_connection_str, relation_name)
+            ysql_dump_cmd = password_prefix_str + ("ysql_dump %s -t %s -s" % (ysql_connection_str, relation_name))
+
             outmsg = get_process_output(ysql_dump_cmd)
             with open(ddl_tmp_file_name, 'a') as ddl_tmp_file:
                 ddl_tmp_file.write(outmsg)
     else:
-        ysql_dump_cmd = password_prefix_str + "ysql_dump %s -s" % (ysql_connection_str)
+        ysql_dump_cmd = password_prefix_str + "ysql_dump %s -s" % ysql_connection_str
         outmsg = get_process_output(ysql_dump_cmd)
         with open(ddl_tmp_file_name, 'w') as ddl_tmp_file:
             ddl_tmp_file.write(outmsg)
-
     with open(ddl_file_name, 'w') as ddl_file:
         with open(ddl_tmp_file_name, 'r') as ddl_tmp_file:
             for line in ddl_tmp_file:
                 if line.strip():
-                    m = re.match(r"(?:^--)|(?:^SET)|(?:^SELECT pg_catalog)|(?:^ALTER TABLE [\w\.]+ OWNER TO)", line)
+                    m = re.match("(?:^--)|(?:^SET)|(?:^SELECT pg_catalog)|(?:^ALTER TABLE [\w\.]+ OWNER TO)", line)
+
                     if m is None:
                         ddl_file.write(line)
         ddl_file.write('\n')
     os.remove(ddl_tmp_file_name)
 
+
 def export_query_file(sql_file, out_dir):
-    query_file_name = out_dir + '/' + QUERY_FILE_NAME
-    print("Exporting query file to %s" % query_file_name)
+    query_file_name = f'{out_dir}/{QUERY_FILE_NAME}'
+    print(f"Exporting query file to {query_file_name}")
     shutil.copy(sql_file, query_file_name)
 
+
 def export_query_plan(cursor, sql_file, out_dir, enable_optimizer_statistics):
-    query_plan_file_name = out_dir + '/' + QUERY_PLAN_FILE_NAME
-    print("Exporting query plan to %s" % query_plan_file_name)
+    query_plan_file_name = f'{out_dir}/{QUERY_PLAN_FILE_NAME}'
+    print(f"Exporting query plan to {query_plan_file_name}")
     with open(sql_file, 'r') as sql_f:
         sql_text = sql_f.read()
 
     if enable_optimizer_statistics:
         cursor.execute('SET yb_enable_optimizer_statistics=ON')
-
-    explain_query = "EXPLAIN %s" % sql_text
+    explain_query = f"EXPLAIN {sql_text}"
     cursor.execute(explain_query)
     query_plan = cursor.fetchall()
     with open(query_plan_file_name, 'w') as query_plan_file:
         for tuple in query_plan:
             query_plan_file.write(tuple[0] + '\n')
 
+
 # Custom JSON encoder to improve readability of statistics.json file
 # Derived from https://stackoverflow.com/a/13252112
 class NoIndent(object):
     """ Value wrapper. """
+
     def __init__(self, value):
         self.value = value
+
 
 class CustomIndentEncoder(json.JSONEncoder):
     FORMAT_SPEC = '@@{}@@'
@@ -182,7 +210,7 @@ class CustomIndentEncoder(json.JSONEncoder):
 
     def __init__(self, **kwargs):
         # Save copy of any keyword argument values needed for use here.
-        self.__sort_keys = kwargs.get('sort_keys', None)
+        self.__sort_keys = kwargs.get('sort_keys')
         super(CustomIndentEncoder, self).__init__(**kwargs)
 
     def default(self, obj):
@@ -190,50 +218,43 @@ class CustomIndentEncoder(json.JSONEncoder):
                 else super(CustomIndentEncoder, self).default(obj))
 
     def encode(self, obj):
-        format_spec = self.FORMAT_SPEC  # Local var to expedite access.
+        format_spec = self.FORMAT_SPEC   # Local var to expedite access.
         json_repr = super(CustomIndentEncoder, self).encode(obj)  # Default JSON.
 
         # Replace any marked-up object ids in the JSON repr with the
         # value returned from the json.dumps() of the corresponding
         # wrapped Python object.
         for match in self.regex.finditer(json_repr):
-            # see https://stackoverflow.com/a/15012814/355230
             id = int(match.group(1))
             no_indent = PyObj_FromPtr(id)
             json_obj_repr = json.dumps(no_indent.value, sort_keys=self.__sort_keys)
 
             # Replace the matched id string with json formatted representation
             # of the corresponding Python object.
-            json_repr = json_repr.replace(
-                            '"{}"'.format(format_spec.format(id)), json_obj_repr)
+            json_repr = json_repr.replace(f'"{format_spec.format(id)}"', json_obj_repr)
 
         return json_repr
 
+
 def export_statistics(cursor, relation_names, out_dir):
-    statistics_file_name = out_dir + '/' + STATISTICS_FILE_NAME
-    print("Exporting data from pg_statistic and pg_class to file %s" % statistics_file_name)
+    statistics_file_name = f'{out_dir}/{STATISTICS_FILE_NAME}'
+    print(f"Exporting data from pg_statistic and pg_class to file {statistics_file_name}")
 
     relation_names_filter = ""
     if relation_names:
-        relation_names_str = ', '.join(['\'{}\''.format(relation_name) for relation_name in relation_names])
-        relation_names_filter = " and c.relname in (%s) " % relation_names_str
+        relation_names_str = ', '.join([f"\'{relation_name}\'" for relation_name in relation_names])
 
-    statistics_dict = {}
-
+        relation_names_filter = f" and c.relname in ({relation_names_str}) "
     query = """
         SELECT row_to_json(t) FROM 
             (SELECT c.relname, c.reltuples, n.nspname
                 FROM pg_class c JOIN pg_namespace n on c.relnamespace = n.oid and n.nspname = 'public' %s) t
-        """ % (relation_names_filter);
-    
+        """ % relation_names_filter
+
     cursor.execute(query)
     rows_pg_class = cursor.fetchall()
-    list_pg_class = []
-    for row in rows_pg_class:
-        list_pg_class.append(NoIndent(row[0]))
-    statistics_dict['pg_class'] = list_pg_class
-
-    # We fetch everything from pg_statistic except starelid and staattnum
+    list_pg_class = [NoIndent(row[0]) for row in rows_pg_class]
+    statistics_dict = {'pg_class': list_pg_class}
     query = """
         SELECT row_to_json(t) FROM 
             (SELECT 
@@ -270,31 +291,30 @@ def export_statistics(cursor, relation_names, out_dir):
                     JOIN pg_statistic s ON s.starelid = c.oid 
                     JOIN pg_attribute a ON c.oid = a.attrelid AND s.staattnum = a.attnum
                     JOIN pg_type t ON a.atttypid = t.oid) t
-        """ % (relation_names_filter)
+        """ % relation_names_filter
 
     cursor.execute(query)
-
     list_pg_statistic = []
     while True:
-        pg_statistic_rows = cursor.fetchmany(STATISTICS_FETCH_ROWS_SIZE)
-        if not pg_statistic_rows:
+        if pg_statistic_rows := cursor.fetchmany(STATISTICS_FETCH_ROWS_SIZE):
+            list_pg_statistic.extend(NoIndent(row[0]) for row in pg_statistic_rows)
+        else:
             break
-
-        for row in pg_statistic_rows:
-            list_pg_statistic.append(NoIndent(row[0]))
-
     statistics_dict['pg_statistic'] = list_pg_statistic
-    statistics_json = json.dumps(statistics_dict, indent = 4, cls=CustomIndentEncoder)
+    statistics_json = json.dumps(statistics_dict, indent=4, cls=CustomIndentEncoder)
+
     with open(statistics_file_name, 'w') as statistics_file:
         statistics_file.write(statistics_json)
 
+
 def set_extra_float_digits(cursor, digits):
-    cursor.execute('SET extra_float_digits = 3')
+    cursor.execute(f'SET extra_float_digits = {digits}')
+
 
 def main():
     args = parse_cmd_line()
-    connectionDict = get_connection_dict(args)
-    conn, cursor = connect_database(connectionDict)
+    connection_dict = get_connection_dict(args)
+    conn, cursor = connect_database(connection_dict)
     Path(args.out_dir).mkdir(parents=True, exist_ok=True)
     out_dir_abs_path = os.path.abspath(args.out_dir)
     relation_names = []
@@ -303,10 +323,11 @@ def main():
         export_query_file(args.sql_file, out_dir_abs_path)
         export_query_plan(cursor, args.sql_file, out_dir_abs_path, args.enable_optimizer_statistics)
     set_extra_float_digits(cursor, 3)
-    extract_ddl(cursor, connectionDict, relation_names, out_dir_abs_path)
+    extract_ddl(connection_dict, relation_names, out_dir_abs_path)
     export_statistics(cursor, relation_names, out_dir_abs_path)
     cursor.close()
     conn.close()
+
 
 if __name__ == "__main__":
     main()
